943747ebf48028cb45081f5f50c0476b41d5cef7
louktao014 <looktaotsk@gmail.com>
2026-06-26 09:14:25 +0700

    feat: add audio and meeting services, implement transcript component and upload audio functionality
    - Created AudioService for handling audio file uploads and conversions.
    - Implemented HistoryService to manage user history with signals.
    - Developed MeetingDataService to mock meeting transcripts and minutes of meeting (MoM).
    - Added MeetingService for generating MoM from AI models.
    - Introduced TranscriptService for managing transcript operations.
    - Created TranscriptComponent for displaying and editing transcripts.
    - Implemented UploadAudioComponent for file upload with drag-and-drop support.
    - Added HTML and SCSS files for transcript and upload audio components.
    - Set up global styles and index files for the frontend application.
    - Configured TypeScript settings for the project.

---
+++ packages/frontend/.angular/cache/21.1.0/myapp/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,14 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-OJWPKCXE.js";
+import "./chunk-QFMROOX3.js";
+import "./chunk-WO37JHK7.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
---
+++ packages/frontend/.angular/cache/21.1.0/myapp/vite/deps/@angular_material_button.js
@@ -0,0 +1,34 @@
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-BHWOQ7L5.js";
+import "./chunk-RKSMQ2C2.js";
+import "./chunk-OJWPKCXE.js";
+import "./chunk-KHEPOWN4.js";
+import "./chunk-S64TTIEY.js";
+import "./chunk-ESZE5QY5.js";
+import "./chunk-HCL3TUYO.js";
+import "./chunk-QFMROOX3.js";
+import "./chunk-WO37JHK7.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
